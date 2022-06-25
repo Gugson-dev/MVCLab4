28dd7310ab6520831028b1b34afe02fc4e63ffac
Daniel61538 <d.szypniewski.kontakt@gmail.com>
2022-06-25 10:05:40 +0200

    Add project files.

---
+++ Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCLab4.Models;
+
+namespace MVCLab4.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            var model = new Class1();
+            model.Imie = "Daniel";
+            model.Nazwisko = "Szypniewski";
+            return View(model);
+        }
+    }
+}
---
+++ Models/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCLab4.Models
+{
+    public class Class1
+    {
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+
+    }
+}
